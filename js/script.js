a10bb3db63c6d3f82b6db8a881b2ebe55e799638
danillo.caetano <danillo.caetano@unimedmaceio.com.br>
2022-03-02 17:38:42 -0300

    :tada: first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btnMobile = document.querySelector('#btn-mobile');
+btnMobile.addEventListener('click', () => {
+    const nav = document.querySelector('#nav');
+    nav.classList.toggle('active');
+});
+
+const menuList = document.querySelector('.menu');
+menuList.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
